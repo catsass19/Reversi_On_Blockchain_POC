4ac2249c46a008f49fca1a536f5821ecf0ceb43e
Chen Wei <wei.chen@cobinhood.com>
2018-10-06 18:05:16 +0800

    start writing some contract and test

--- contracts/Deversi.sol
+++ contracts/Deversi.sol
@@ -2,11 +2,85 @@ pragma solidity ^0.4.24;
 
 contract Deversi {
 
+    uint8 public INIT_SIZE = 6;
+    uint32 public INIT_RAISING_PERIOD = 60;
+    uint32 public INIT_TURN_PERIOD = 60;
+    uint256 public INIT_SHARE_PRICE = 1000000000000000;
+    uint8 public INIT_SHARE_GROWTH_RATE = 5;
+    enum _TEAM { BULL, BEAR, NONE }
+
+    struct Ledger {
+        uint256 BULL;
+        uint256 BEAR;
+    }
+    struct User {
+        _TEAM team;
+        Ledger ledger;
+    }
+
+    // System config
+    uint8 public size; // board size
+    address public owner;
+    uint32 public fundRaisingPeriod;
+    uint32 public turnPeriod;
+    uint256 public baseSharePrice;
+    uint8 public shareGrowthRate;
+
+    // Game control
+    uint32 public gameRound;
+    uint16 public currentTurn;
+    uint256 public currentSharePrice;
+    bool public fundRaisingCountingDown;
+    uint256 public countingStartedTime;
+    _TEAM public currentTeam;
+
+    mapping(uint32 => mapping(address => User)) public userStatus;
+
     string public myString = "Hello World2";
     event StringUpdated();
+
+    constructor() public {
+        owner = msg.sender;
+        gameRound = 0;
+        configure(
+            INIT_SIZE,
+            INIT_RAISING_PERIOD,
+            INIT_TURN_PERIOD,
+            INIT_SHARE_PRICE,
+            INIT_SHARE_GROWTH_RATE
+        );
+        startNewGame();
+    }
+
+    function startNewGame() private {
+        gameRound = gameRound + 1;
+        currentTurn = 0;
+        currentSharePrice = baseSharePrice;
+        fundRaisingCountingDown = false;
+        currentTeam = _TEAM.NONE;
+    }
 
     function set(string x) public {
         myString = x;
         emit StringUpdated();
+    }
+
+    function configure(
+        uint8 _size,
+        uint32 _fundRaisingPeriod,
+        uint32 _turnPeriod,
+        uint256 _baseSharePrice,
+        uint8 _shareGrowthRate
+    ) public onlyOwner {
+        size = _size;
+        fundRaisingPeriod = _fundRaisingPeriod;
+        turnPeriod = _turnPeriod;
+        baseSharePrice = _baseSharePrice;
+        shareGrowthRate = _shareGrowthRate;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner is allowed");
+        _;
     }
 }
---
+++ test/Deversi.js
@@ -0,0 +1,68 @@
+const DeversiContract = artifacts.require("./Deversi.sol");
+
+contract("Deversi", accounts => {
+  it("should store the string 'Hey there!'", async () => {
+    const Deversi = await DeversiContract.deployed();
+
+    // Set myString to "Hey there!"
+    await Deversi.set("Hey there!", { from: accounts[0] });
+
+    // Get myString from public variable getter
+    const storedString = await Deversi.myString.call();
+
+    assert.equal(storedString, "Hey there!", "The string was not stored");
+  });
+  it('Make sure game initialization is correct', async () => {
+    const Deversi = await DeversiContract.deployed();
+    const owner = await Deversi.owner.call();
+    const gameRound = await Deversi.gameRound.call();
+    const init_size = await Deversi.INIT_SIZE.call();
+    const init_raising_period = await Deversi.INIT_RAISING_PERIOD.call();
+    const init_turn_period = await Deversi.INIT_TURN_PERIOD.call();
+    const init_share_price = await Deversi.INIT_SHARE_PRICE.call();
+    const init_share_growth_rate = await Deversi.INIT_SHARE_GROWTH_RATE.call();
+    const size = await Deversi.size.call();
+    const fundRaisingPeriod = await Deversi.fundRaisingPeriod.call();
+    const turnPeriod = await Deversi.turnPeriod.call();
+    const baseSharePrice = await Deversi.baseSharePrice.call();
+    const shareGrowthRate = await Deversi.shareGrowthRate.call();
+
+    assert.equal(owner, accounts[0], 'owner is not set to contract initiator');
+    assert.equal(gameRound, 1, 'game round is not set to 1');
+    assert.equal(size.toFixed(), init_size.toFixed(), 'size is not set to init value');
+    assert.equal(fundRaisingPeriod.toFixed(), init_raising_period.toFixed(), 'fundRaisingPeriod is not set to init value');
+    assert.equal(turnPeriod.toFixed(), init_turn_period.toFixed(), 'turnPeriod is not set to init value');
+    assert.equal(baseSharePrice.toFixed(), init_share_price.toFixed(), 'baseSharePrice is not set to init value');
+    assert.equal(shareGrowthRate.toFixed(), init_share_growth_rate.toFixed(), 'shareGrowthRate is not set to init value');
+  });
+  it('non-Owner should not be able to configure', async() => {
+    const Deversi = await DeversiContract.deployed();
+    try {
+      await Deversi.configure(
+        '8', '61', '61', '2000000000000000', '6',
+        { from: accounts[1] }
+      );
+    } catch(e) {
+      assert.isDefined(e, 'Error is not present');
+      return;
+    }
+    throw new Error('should not get here');
+  });
+  it('Owner should be able to configure', async() => {
+    const Deversi = await DeversiContract.deployed();
+    await Deversi.configure(
+      '8', '61', '62', '2000000000000000', '6',
+      { from: accounts[0] }
+    );
+    const size = await Deversi.size.call();
+    const fundRaisingPeriod = await Deversi.fundRaisingPeriod.call();
+    const turnPeriod = await Deversi.turnPeriod.call();
+    const baseSharePrice = await Deversi.baseSharePrice.call();
+    const shareGrowthRate = await Deversi.shareGrowthRate.call();
+    assert.equal(size.toFixed(), '8', 'size is not configured');
+    assert.equal(fundRaisingPeriod.toFixed(), '61', 'fundRaisingPeriod is not configured');
+    assert.equal(turnPeriod.toFixed(), '62', 'turnPeriod is not configured');
+    assert.equal(baseSharePrice.toFixed(), '2000000000000000', 'baseSharePrice is not configured');
+    assert.equal(shareGrowthRate.toFixed(), '6', 'shareGrowthRate is not configured');
+  });
+});
